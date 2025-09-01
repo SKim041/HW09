dae4829687e26991fb65c3872d9fa99a433e35c1
SKim041 <sehee1806@gmail.com>
2025-09-01 14:25:32 +0900

    feat(Game,Player): 필요 클래스 생성

---
+++ Source/BullsAndCows/Game/BACGameModeBase.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BACGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BULLSANDCOWS_API ABACGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};
---
+++ Source/BullsAndCows/Player/BACPlayerState.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "BACPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BULLSANDCOWS_API ABACPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+};
