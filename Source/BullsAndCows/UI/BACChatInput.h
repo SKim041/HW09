bdd6c08bf16e7732af4a17065b332c7fa4edb21b
SKim041 <sehee1806@gmail.com>
2025-09-01 14:31:23 +0900

    feat(UI): 위젯 C++ 클래스 및 모듈 추가

---
+++ Source/BullsAndCows/UI/BACChatInput.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BACChatInput.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BULLSANDCOWS_API UBACChatInput : public UUserWidget
+{
+	GENERATED_BODY()
+};
